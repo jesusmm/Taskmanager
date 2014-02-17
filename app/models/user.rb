e358045a7d2d43afd61592331a258637a76f3edf
root <jesusmm2p@gmail.com>
2014-02-16 21:58:35 -0500

    Punto 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :apellidos, :correo, :nombre
+  validates :nombre, presence: true
+  validates :apellidos, presence: true
+  validates :correo, presence: true
 end
